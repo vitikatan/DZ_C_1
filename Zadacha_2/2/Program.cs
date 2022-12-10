851c4bc5d0da4ffdaee32c4859eee1edb28ceee7
yana <wyemelt@gmail.com>
2022-12-10 23:18:15 +0300

    Zadacha_2 done

---
+++ Zadacha_2/2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear ();
+int number1 = 2;
+int number2 = 10;
+if (number1 > number2)
+{
+    Console.WriteLine ($"Число {number1} больше");
+}
+else 
+{
+    Console.WriteLine($"Число {number2} больше");
+}
---
+++ Zadacha_2/3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear ();
+int number1 = -9;
+int number2 = -3;
+if (number1 > number2)
+{
+    Console.WriteLine ($"Число {number1} больше");
+}
+else 
+{
+    Console.WriteLine($"Число {number2} больше");
+}
