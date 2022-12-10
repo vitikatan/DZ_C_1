f5f4c51de14a68bd82d82190f1ef36892a2fc788
yana <wyemelt@gmail.com>
2022-12-11 00:05:18 +0300

    Zadacha_6 done

---
+++ Zadacha_6/3/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear ();
+int number1 = 7;
+if ((number1 % 2) == 0)
+{
+    Console.WriteLine ($"Число {number1} четное");
+}
+else
+{
+    Console.WriteLine ($"Число {number1} не четное");
+}
