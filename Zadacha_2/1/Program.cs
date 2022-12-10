2f934ba453dc2a5c669d2a6bad15b269486a386a
yana <wyemelt@gmail.com>
2022-12-10 23:11:55 +0300

    init commit

---
+++ Zadacha_2/1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear ();
+int number1 = 5;
+int number2 = 7;
+if (number1 > number2)
+{
+    Console.WriteLine ($"Число {number1} больше");
+}
+else 
+{
+    Console.WriteLine($"Число {number2} больше");
+}
